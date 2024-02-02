3d2c6fc48f235c3fe472325f659bca5440d46e0e
ArmaG0Re <148721782+ArmaG0Re@users.noreply.github.com>
2024-02-02 16:33:20 -0600

    Se cambió el tipo de la propiedad Categoria y se creó el enum CategoriaNivel

--- TesisActividades/Program.cs
+++ TesisActividades/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+Desarrollador desarrollador = new Desarrollador();
+desarrollador.Nombre = "Armando Gonzalez";
+desarrollador.Categoria = CategoriaNivel.Nivel1;
 
 public class Desarrollador
 {
     public string Nombre { get; set; }
     public DateTime FechaIngreso { get; set; }
     public string Email { get; set; }
-    public int Categoria { get; set; }
+    public CategoriaNivel Categoria { get; set; }
     public List<Proyecto> Proyectos { get; set; }
 
     // Métodos para asignar proyecto.
@@ -20,11 +21,11 @@ public class Desarrollador
 public class Proyecto
 {
     public string Nombre { get; set; }
-    public int Categoria { get; set; }
+    public CategoriaNivel Categoria { get; set; }
     public int DuracionHoras { get; set; }
     public DateTime FechaInicio { get; set; }
-    public Dictionary<DateTime, int> HorasTrabajadasPorDia { get; set; }
     public Desarrollador Desarrollador { get; set; }
+    public Dictionary<DateTime, int> HorasTrabajadasPorDia { get; set; }
 
     // Método para registrar horas trabajadas.
     public void RegistrarHoras(DateTime fecha, int horas)
@@ -47,3 +48,9 @@ public class Proyecto
     }
 }
 
+public enum CategoriaNivel 
+{
+    Nivel1 = 1,
+    Nivel2 = 2,
+    Nivel3 = 3
+}
